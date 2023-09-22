72acd54ce19ed7a148fc67144c3289b9a680f5da
nguyenducduyyy <80279493+nguyenducduyyy@users.noreply.github.com>
2023-09-22 21:16:39 +0700

    ignore env

--- server/config/db/index.js
+++ server/config/db/index.js
@@ -1,5 +1,7 @@
 
 const mongoose = require('mongoose')
+const MONGODB_URL = process.env.MONGODB_URL;
+console.log(MONGODB_URL);
 async function connect() {
     // try{
     //     await mongoose.connect('mongodb://127.0.0.1/web_shopet',
@@ -12,7 +14,7 @@ async function connect() {
     //     console.log('failure');
     // }
     mongoose.set('strictQuery', false);
-    mongoose.connect('mongodb://127.0.0.1/web_shopet', { useNewUrlParser: true })
+    mongoose.connect(MONGODB_URL, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Could not connect to MongoDB', err));
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express();
-// const dotenv = require("dotenv")
-// dotenv.config()
+const dotenv = require("dotenv")
+dotenv.config()
 
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -48,7 +48,7 @@ db.connect();
 
 
 routers(app);
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`SERVER is running on port ${PORT}`);
 });
